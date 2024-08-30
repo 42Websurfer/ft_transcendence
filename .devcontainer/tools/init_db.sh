d59d11a3bbd72e455b3d9b831cd3136f80137b7f
Nikster3k <144370425+Nikster3k@users.noreply.github.com>
2024-08-30 12:56:19 +0200

    FEAT: django/devcontainer, nginx container, postgres container

--- .devcontainer/tools/init_db.sh
+++ .devcontainer/tools/init_db.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-service postgresql start
-
-sleep 5
-
-su - postgres -c "psql <<EOF
-CREATE DATABASE $DATABASE_NAME;
-CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PW';
-GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME TO $DATABASE_USER;
-EOF"
-
 python3 -m venv /workspaces/app/webappenv
 
 source /workspaces/app/webappenv/bin/activate
@@ -18,4 +8,4 @@ pip install django psycopg2-binary
 /workspaces/app/transendence/manage.py makemigrations
 /workspaces/app/transendence/manage.py migrate
 
-nginx -g "daemon off;"
+python3 /workspaces/app/transendence/manage.py runserver 0.0.0.0:8000
