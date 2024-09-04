9cd333a6c139909fdaebb98e850efcad26ffd216
windchaser-surf <wechslerf@gmail.com>
2024-09-04 12:46:40 +0000

    ADD: whitenoice to python environemnt

--- .devcontainer/tools/init_django.sh
+++ .devcontainer/tools/init_django.sh
@@ -7,5 +7,6 @@ source /workspaces/app/webappenv/bin/activate
 pip install django psycopg2-binary
 /workspaces/app/transendence/manage.py makemigrations
 /workspaces/app/transendence/manage.py migrate
+pip install whitenoise
 
 python3 /workspaces/app/transendence/manage.py runserver 0.0.0.0:8000
