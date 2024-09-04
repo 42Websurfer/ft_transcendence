9f71a0079abb7df1619c3a37783b29541e7b950a
windchaser-surf <wechslerf@gmail.com>
2024-09-04 12:24:37 +0000

    FIX: larger size for password length

--- app/transendence/pong/models.py
+++ app/transendence/pong/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     lastname = models.CharField(max_length=255)
     username = models.CharField(max_length=255, default="DEFAULT")
     email = models.EmailField(default="default@email.com")
-    password = models.CharField(max_length=100, default="DEFAULT")
+    password = models.CharField(max_length=255, default="DEFAULT")
 
     def __str__(self):  #describes the user instance if for example show all objects,
         return f"{self.firstname} {self.lastname}"
