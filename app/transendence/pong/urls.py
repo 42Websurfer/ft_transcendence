24bb8ca4d790a679ac07f7b16e1f33258f764567
windchaser-surf <wechslerf@gmail.com>
2024-09-05 11:03:35 +0000

    FIX: register handler checks if email and username already exists

--- app/transendence/pong/urls.py
+++ app/transendence/pong/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getLogin, name='getLogin'),
+    path('', views.register, name='register'),
 ]
--- app/transendence/pong/views.py
+++ app/transendence/pong/views.py
@@ -4,16 +4,56 @@ from django.template import loader
 from .models import User
 from .forms import UserForm
 
-def pong(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
+#from django.views.decorators.csrf import csrf_protect
 
-    form = UserForm()
-    users = User.objects.all()
-    return render(request, 'niko.html', {'form': form, 'pongUser': users})
+# def pong(request):
+#     if request.method == 'POST':
+#         form = UserForm(request.POST)
+#         if form.is_valid():
+#             form.save()
 
-def getLogin(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+#     form = UserForm()
+#     users = User.objects.all()
+#     return render(request, 'niko.html', {'form': form, 'pongUser': users})
+
+#@csrf_protect
+def login(request):
+	if request.method == 'POST':
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+		return HttpResponse(f"Received password: {password}, email: {email}")
+	elif request.method == 'GET':
+		form = UserForm()
+		return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+	if request.method == 'POST':
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+		firstname = request.POST.get('firstname')
+		lastname = request.POST.get('lastname')
+		username = request.POST.get('username')
+		form = UserForm(request.POST)
+		if form.is_valid():
+			if User.objects.filter(email=email).exists():
+				return HttpResponse("EMAIL EXISTS ALREADY!!!")
+			elif User.objects.filter(username=username).exists():
+				return HttpResponse("USERNAME EXISTS ALREADY")
+			else:
+				form.save()
+			return HttpResponse(f"IT GOES WELLLL!!! Email: {email}, password: {password}, firstname: {firstname}, lastname: {lastname}, username: {username}")
+		else:
+			return HttpResponse(f"IT GOES WROOOOONG!!! Email: {email}, password: {password}, firstname: {firstname}, lastname: {lastname}, username: {username}")
+
+	elif request.method == 'GET':
+		return render(request, 'register.html')
+	# def login(request):
+#     if request.method == 'GET':
+#         template = loader.get_template('login.html')
+#         return render(request, 'login.html', {})
+#     elif request.method == 'POST':
+#         form = UserForm(request.POST)
+#         if form is_valid()
+#             print
+#         return render(request, 'test.html', {})
