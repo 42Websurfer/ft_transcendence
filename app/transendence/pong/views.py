d45fe009332fc770722f3d7b634ae2588f0d1c7a
Felix <wechslerf@gmail.com>
2024-09-07 20:40:29 +0200

    FIX: changed the login logout and register function, that they receive and response in json format

--- app/transendence/pong/views.py
+++ app/transendence/pong/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password
+from django.views.decorators.csrf import csrf_exempt
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 def startpage(request):
 	if request.user.is_authenticated:
@@ -12,48 +18,85 @@ def startpage(request):
 	else:
 		return render (request, 'login.html')
 
+def check_auth(request):
+	if request.user.is_authenticated:
+		return JsonResponse({
+			'authenticated': True,
+			'user': {
+				'id': request.user.id,
+				'username': request.user.username,
+				'email': request.user.email,
+				'first_name': request.user.first_name,
+				'last_name': request.user.last_name
+			}
+		})
+	else:
+		return JsonResponse({'authenticated': False})
+
+@csrf_exempt
 def user_login(request):
 	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		if User.objects.filter(username=username).exists():
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				return redirect('/')
+		try: 
+			data = json.loads(request.body)
+			username = data.get('username')
+			password = data.get('password')
+			logger.debug(f"Received username: {username}")
+			logger.debug(f"Received password: {password}")
+			if User.objects.filter(username=username).exists():
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					login(request, user)
+					return JsonResponse({
+						'success': 'User logged in successfully.',
+						'user': {
+							'id': user.id,
+							'username': user.username,
+							'email': user.email,
+							'first_name': user.first_name,
+							'last_name': user.last_name
+						}
+					}, status=200)
+				else:
+					return JsonResponse({'error': 'Invalid password.'}, status=400)
 			else:
-				messages.error(request, "Invalid Password")
-				return render(request, 'login.html')
-		else:
-			messages.error(request, "Invalid Username")
-			return render(request, 'login.html')
-	elif request.method == 'GET':
-		return render(request, 'login.html')
+				return JsonResponse({'error': 'User does not exist.'}, status=400)
+		except json.JSONDecodeError:
+			return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+	return JsonResponse({'error': 'Invalid request method.'}, status=405)
 
 def user_logout(request):
 	if request.user.is_authenticated: 
 		logout(request)
-	return redirect('/')
-
+		return JsonResponse({
+			'success': 'User logged in successfully.'
+		}, status=200)
+@csrf_exempt
 def register(request):
 	if request.method == 'POST':
-		email = request.POST.get('email')
-		password = request.POST.get('password')
-		firstname = request.POST.get('firstname')
-		lastname = request.POST.get('lastname')
-		username = request.POST.get('username')
+		try:
+			data = json.loads(request.body)		
+			email = data.get('email')
+			password = data.get('password')
+			firstname = data.get('firstname')
+			lastname = data.get('lastname')
+			username = data.get('username')
 
-		if User.objects.filter(email=email).exists():
-			messages.error(request, 'Email address already exists.')
-			return render(request, 'register.html')			
-		elif User.objects.filter(username=username).exists():
-			messages.error(request, 'Username already exists.')
-			return render(request, 'register.html')
-		user = User.objects.create_user(username=username, email=email, first_name=firstname, last_name=lastname)
-		user.set_password(password)
-		user.save()
-		login(request, user)
-		return redirect('/')
-
-	elif request.method == 'GET':
-		return render(request, 'register.html')
+			if User.objects.filter(email=email).exists():
+				return JsonResponse({'error': 'Email address already exists.'}, status=400)
+			elif User.objects.filter(username=username).exists():
+				return JsonResponse({'error': 'Username already exists.'}, status=400)
+			user = User.objects.create_user(username=username, email=email, first_name=firstname, last_name=lastname)
+			user.set_password(password)
+			user.save()
+			return JsonResponse({
+				'success': 'User registered successfully.',
+				'user': {
+					'id': user.id,
+					'username': user.username,
+					'email': user.email,
+					'first_name': user.first_name,
+					'last_name': user.last_name
+				}
+			}, status=201)
+		except json.JSONDecodeError:
+					return JsonResponse({'error': 'Invalid JSON.'}, status=400)
