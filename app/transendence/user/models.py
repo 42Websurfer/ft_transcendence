3ebfc4413597d8ada4f98ecf6cbfacb282795832
Felix <wechslerf@gmail.com>
2024-09-16 16:43:25 +0200

    ADD: Friendship model and removed old user model

--- app/transendence/user/models.py
+++ app/transendence/user/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-class User(models.Model):
-    email = models.EmailField(default="default@email.com")
-    password = models.CharField(max_length=255, default="DEFAULT")
-    firstname = models.CharField(max_length=100, default="DEFAULT")
-    lastname = models.CharField(max_length=100, default = "DEFAULT")
-    username = models.CharField(max_length=100, default = "DEFAULT")
-    
+User = get_user_model()
+
+class Friendship(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+
+    user = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together=('user', 'friend')
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.friend.username} ({self.status})"
