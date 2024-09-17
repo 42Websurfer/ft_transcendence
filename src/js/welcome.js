349818b1e2c905bdc55e5a923bde3908f9348a31
windchaser-surf <wechslerf@gmail.com>
2024-09-17 14:19:23 +0200

    FIX: test environment for add a match and show all played matches

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,4 +1,4 @@
-import { getCookie } from './utils.js';
+import { getCookie, displayMessages } from './utils.js';
 
 export function renderWelcome() {
     const app = document.getElementById('app');
@@ -13,6 +13,33 @@ export function renderWelcome() {
     <h2>Accpeted Friend's </h2>
     <button id="acceptedFriendButton">Show Accepted Friend's</button>
     <div id="acceptedFriend"></div>
+    <br>
+    <hr>
+    <form id="matchForm">
+        <input type="hidden" name="csrfmiddlewaretoken" value="${getCookie('csrftoken')}">
+        <h2>Add Match Results</h2>
+        <div class="form-floating">
+            <input type="text" name="player1" class="form-control" required>
+            <label for="floatingInput">Player 1</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" name="player2" class="form-control" required>
+            <label for="floatingPassword">Player 2</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" name="score_player1" class="form-control" required>
+            <label for="floatingPassword">Score Player 1</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" name="score_player2" class="form-control" required>
+            <label for="floatingPassword">Score Player 2</label>
+        </div>
+        <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+	</form>
+    <div id="messages"></div>
+    <h2>Show Match History</h2>
+    <button id="matchHistoryButton">Show Matches</button>
+    <div id="matchHistory"></div>
     `;
     const friendRequestButton = document.getElementById('sendFriendRequest');
     const responseDisplay = document.getElementById('responseDisplay');
@@ -20,6 +47,12 @@ export function renderWelcome() {
     const checkRequestsButton = document.getElementById('checkRequests');
     const acceptedRequests = document.getElementById('acceptedFriend');
     const acceptedFriendButton = document.getElementById('acceptedFriendButton');
+    const formdata = document.getElementById('matchForm');
+    const messages = document.getElementById('messages');
+    const matchHistoryButton = document.getElementById('matchHistoryButton');
+    const matchHistory = document.getElementById('matchHistory');
+
+    formdata.addEventListener('submit', handleMatchFormSubmit);
 
     friendRequestButton.addEventListener('click', async () => {
         const friendId = prompt("Enter the friend's user ID:");
@@ -39,15 +72,43 @@ export function renderWelcome() {
         displayFriendListResponse(response);
     })
 
+    matchHistoryButton.addEventListener('click', async () => {
+        const response = await getMatchHistory();
+        displayMatchHistory(response);
+    })
+
+    async function handleMatchFormSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        const data = {
+            player1_id: formData.get('player1'),
+            player2_id: formData.get('player2'),
+            score_player1: formData.get('score_player1'),
+            score_player2: formData.get('score_player2'),
+        };
+    
+        const csrftoken = getCookie('csrftoken');
+    
+        const response = await fetch(`/match_result/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify(data)
+        });
+    
+        const result = await response.json();
+        displayMatchResponse(result);
+    
+    }
+
     async function sendFriendRequest(friendId) {
         try {
             const csrftoken = getCookie('csrftoken');
             const response = await fetch(`/send_friend_request/${friendId}/`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrftoken
-                },
+                method: 'GET',
                 credentials: 'include'
             });
             return await response.json();
@@ -80,6 +141,19 @@ export function renderWelcome() {
             return { error: 'Failed to send friend request.' };
         }
     }
+
+    async function getMatchHistory() {
+        try {
+            const response = await fetch(`/match_history/`, {
+                method: 'GET',
+                credentials: 'include'
+            });
+            return await response.json();
+        } catch (error) {
+            return { error: 'Failed to get match histroy.' };
+        }
+    }
+
     function displayResponse(response) {
         responseDisplay.innerHTML = `
             <pre>${JSON.stringify(response, null, 2)}</pre>
@@ -93,6 +167,17 @@ export function renderWelcome() {
     }
     function displayFriendListResponse(response) {
         acceptedRequests.innerHTML = `
+            <pre>${JSON.stringify(response, null, 2)}</pre>
+        `;
+    }
+
+    function displayMatchResponse(response) {
+        messages.innerHTML = `
+            <pre>${JSON.stringify(response, null, 2)}</pre>
+        `;
+    }
+    function displayMatchHistory(response) {
+        matchHistory.innerHTML = `
             <pre>${JSON.stringify(response, null, 2)}</pre>
         `;
     }
