eaeda2717aceec3170cd4efc5b8dbbe632a1f8e1
windchaser-surf <wechslerf@gmail.com>
2024-09-19 15:34:14 +0000

    RM: console.log with the data which will be received from backend

--- src/js/login.js
+++ src/js/login.js
@@ -35,7 +35,6 @@ async function handleLoginFormSubmit(event)
 		username: formData.get('login-username'),
         password: formData.get('login-password')
     };
-	console.log('DATA: ', data);
     const csrftoken = getCookie('csrftoken');
 
     const response = await fetch('/login/', {
