859692335abe29fc3516417ee79cc6534f29ffc2
windchaser-surf <wechslerf@gmail.com>
2024-09-19 15:31:13 +0000

    ADD: UserStatus consumer class update the user online status, and will be update if someone connect or disconnects

--- app/transendence/game/consumers.py
+++ app/transendence/game/consumers.py
@@ -1,7 +1,10 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
+import redis
 import logging
+from asgiref.sync import sync_to_async
+from django.contrib.auth import get_user_model
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -114,3 +117,65 @@ self.scope:
 -	await self.accept():
 Diese Methode akzeptiert die eingehende WebSocket-Verbindung. Ohne diesen Aufruf wird die Verbindung nicht hergestellt.
  """
+
+redisStore = redis.Redis(host='redis', port=6379, db=0)
+
+class UserStatus(AsyncWebsocketConsumer):
+	async def connect(self):
+		self.group_name = 'online_status'
+		self.user = self.scope["user"]
+		if (self.user.is_authenticated):
+			await self.channel_layer.group_add(
+				self.group_name,
+				self.channel_name
+			)
+			await self.accept()
+			redisStore.sadd('online_users', self.user.id)
+			await self.channel_layer.group_send(
+				self.group_name,
+				{
+					'type': 'send_online_users',
+				}
+			)
+		else:
+			print("NOT AUTHENTICATED")
+		
+	async def disconnect(self, close_code):
+		if (self.user.is_authenticated):
+			await self.channel_layer.group_discard(
+				self.group_name,
+				self.channel_name
+			)
+			redisStore.srem('online_users', self.user.id)
+			await self.channel_layer.group_send(
+				self.group_name,
+				{
+					'type': 'send_online_users',
+				}
+			)
+
+
+	async def send_online_users(self, event):
+		try:
+			User = get_user_model()
+			online_users_ids = redisStore.smembers("online_users")
+
+			online_users_ids = [int(user_id) for user_id in online_users_ids]
+
+			online_users = await sync_to_async(list)(User.objects.filter(id__in=online_users_ids))
+
+			user_data = [
+				{
+					'id': user.id,
+					'username': user.username,
+				}
+				for user in online_users
+			]
+			for user in online_users:
+				print(user_data)
+				print(user.id)
+				print(user.username)
+			
+			await self.send(text_data=json.dumps({'online_users': user_data}))
+		except Exception as e:
+			print("ERRRRRRRRROR")
