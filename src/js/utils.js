120d3addf94d4a47815ee89c46264fe437bfa7ea
windchaser-surf <wechslerf@gmail.com>
2024-09-19 15:34:53 +0000

    ADD: close ws when user logout

--- src/js/utils.js
+++ src/js/utils.js
@@ -42,6 +42,9 @@ export async function handleLogoutSubmit()
     });
 
     const result = await response.json();
+    if (ws)
+        ws.close(1000, "Client closed coonection");
+    wsBool = false;
     showSection('login');
 }
 
