2024f593a1598c03ed142e2a040fefa7c264e6f2
windchaser-surf <wechslerf@gmail.com>
2024-09-19 15:31:31 +0000

    ADD: online-status path

--- app/transendence/game/routing.py
+++ app/transendence/game/routing.py
@@ -3,4 +3,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/pong/(?P<group_name>\w+)/$', consumers.MyConsumer.as_asgi()),
+    re_path(r'ws/online-status/$', consumers.UserStatus.as_asgi()),
+
 ]
--- app/transendence/transendence/routing.py
+++ app/transendence/transendence/routing.py
@@ -3,6 +3,8 @@ from game import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/pong/(?P<group_name>\w+)/$', consumers.MyConsumer.as_asgi()),
+    re_path(r'ws/online-status/$', consumers.UserStatus.as_asgi()),
+
 ]
 
 """ 
