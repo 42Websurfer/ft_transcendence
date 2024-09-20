b0b9f2f3ac6fa30a03d719eec16344a18cc32df3
windchaser-surf <wechslerf@gmail.com>
2024-09-20 16:34:59 +0000

    ADD: the tournament app

--- app/transendence/transendence/asgi.py
+++ app/transendence/transendence/asgi.py
@@ -12,6 +12,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import game.routing
+import tournament.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transendence.settings')
 
@@ -19,7 +20,8 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            game.routing.websocket_urlpatterns
+            game.routing.websocket_urlpatterns +
+            tournament.routing.websocket_urlpatterns
         )
     ),
 })
