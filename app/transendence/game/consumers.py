5624678285259343ba8acc0854cdf854b31c4f71
windchaser-surf <wechslerf@gmail.com>
2024-09-20 16:35:15 +0000

    RM: debugging prints

--- app/transendence/game/consumers.py
+++ app/transendence/game/consumers.py
@@ -2,12 +2,8 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import redis
-import logging
 from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 class MyConsumer(AsyncWebsocketConsumer):  
 	async def connect(self):
@@ -92,12 +88,12 @@ class MyConsumer(AsyncWebsocketConsumer):
 		}))
 
 """ 
-                player1_posX: 1,
-                player1_posY: 0.3,
-                player2_posX: 4,
-                player2_posY: 0.7,
-                ball_posX: 12,
-                ball_posY: 0.02,
+				player1_posX: 1,
+				player1_posY: 0.3,
+				player2_posX: 4,
+				player2_posY: 0.7,
+				ball_posX: 12,
+				ball_posY: 0.02,
  """
 
 """ 
@@ -156,26 +152,25 @@ class UserStatus(AsyncWebsocketConsumer):
 
 
 	async def send_online_users(self, event):
-		try:
-			User = get_user_model()
-			online_users_ids = redisStore.smembers("online_users")
+		#try:
+		User = get_user_model()
+		online_users_ids = redisStore.smembers("online_users")
+		print(f"SELF USER: {self.user}")
+		print(f"SELF USER_id: {self.user.id}")
 
-			online_users_ids = [int(user_id) for user_id in online_users_ids]
-
-			online_users = await sync_to_async(list)(User.objects.filter(id__in=online_users_ids))
-
-			user_data = [
-				{
-					'id': user.id,
-					'username': user.username,
-				}
-				for user in online_users
-			]
-			for user in online_users:
-				print(user_data)
-				print(user.id)
-				print(user.username)
-			
-			await self.send(text_data=json.dumps({'online_users': user_data}))
-		except Exception as e:
-			print("ERRRRRRRRROR")
+		online_users_ids = [int(user_id) for user_id in online_users_ids]
+		#friend_ids = await sync_to_async(list)(Friendship.objects.filter )
+		online_users = await sync_to_async(list)(User.objects.filter(id__in=online_users_ids))
+		online_users_data = [{'id': user.id} for user in online_users]
+		print(f"ALL ONLINE USER:  {online_users_data}")
+		user_data = [
+			{
+				'id': user.id,
+				'username': user.username,
+			}
+			for user in online_users
+		]
+		
+		await self.send(text_data=json.dumps({'online_users': user_data}))
+		# except Exception as e:
+		# 	print("ERRRRRRRRROR")
