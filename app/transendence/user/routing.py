d0688eaaf55cf4f02f18e30c7f62d53677e60013
windchaser-surf <wechslerf@gmail.com>
2024-09-24 16:36:52 +0000

    FIX: changed routing for UserStatus class from game app to user app

--- app/transendence/user/routing.py
+++ app/transendence/user/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/pong/(?P<group_name>\w+)/$', consumers.MyConsumer.as_asgi()),
+    re_path(r'ws/online-status/$', consumers.UserStatus.as_asgi()),
 ]
