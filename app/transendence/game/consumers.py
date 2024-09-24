48dbc82dc29f7e6b311d5ce4eb6ec98f536454af
windchaser-surf <wechslerf@gmail.com>
2024-09-24 16:36:11 +0000

    RM: UserStatus class moved to user app

--- app/transendence/game/consumers.py
+++ app/transendence/game/consumers.py
@@ -105,14 +105,6 @@ class MyConsumer(AsyncWebsocketConsumer):
 
 
 	async def newPlayerMsg(self,event):
-		# if self.player == event.get('player'):
-		# 	print('-------------------')
-		# 	print('PLAYER:')
-		# 	print(self.player)
-		# 	print(event.get('player'))
-		# 	print('------------------')
-		# 	return 
-		# print(f"NEWPLAYER: {self.player}")
 		users_data = {
 				'type': 'newPlayer',
 				'player': event.get('player'),
@@ -167,62 +159,3 @@ self.scope:
 -	await self.accept():
 Diese Methode akzeptiert die eingehende WebSocket-Verbindung. Ohne diesen Aufruf wird die Verbindung nicht hergestellt.
  """
-
-class UserStatus(AsyncWebsocketConsumer):
-	async def connect(self):
-		self.group_name = 'online_status'
-		self.user = self.scope["user"]
-		if (self.user.is_authenticated):
-			await self.channel_layer.group_add(
-				self.group_name,
-				self.channel_name
-			)
-			await self.accept()
-			redis.sadd('online_users', self.user.id)
-			await self.channel_layer.group_send(
-				self.group_name,
-				{
-					'type': 'send_online_users',
-				}
-			)
-		else:
-			print("NOT AUTHENTICATED")
-		
-	async def disconnect(self, close_code):
-		if (self.user.is_authenticated):
-			await self.channel_layer.group_discard(
-				self.group_name,
-				self.channel_name
-			)
-			redis.srem('online_users', self.user.id)
-			await self.channel_layer.group_send(
-				self.group_name,
-				{
-					'type': 'send_online_users',
-				}
-			)
-
-
-	async def send_online_users(self, event):
-		#try:
-		User = get_user_model()
-		online_users_ids = redis.smembers("online_users")
-		print(f"SELF USER: {self.user}")
-		print(f"SELF USER_id: {self.user.id}")
-
-		online_users_ids = [int(user_id) for user_id in online_users_ids]
-		#friend_ids = await sync_to_async(list)(Friendship.objects.filter )
-		online_users = await sync_to_async(list)(User.objects.filter(id__in=online_users_ids))
-		online_users_data = [{'id': user.id} for user in online_users]
-		print(f"ALL ONLINE USER:  {online_users_data}")
-		user_data = [
-			{
-				'id': user.id,
-				'username': user.username,
-			}
-			for user in online_users
-		]
-		
-		await self.send(text_data=json.dumps({'online_users': user_data}))
-		# except Exception as e:
-		# 	print("ERRRRRRRRROR")
