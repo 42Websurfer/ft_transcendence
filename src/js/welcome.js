682da8363cdb920381e90ae71e234b79c81fed7a
floheid98 <florian.heid98@googlemail.com>
2024-09-25 11:06:36 +0000

    ADD: started implementation of friends feature

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,6 +1,43 @@
 import { getCookie, displayMessages } from './utils.js';
 
+function addListItem(content, ul)
+{
+    const li = document.createElement('li');
+    li.textContent = content;
+    ul.appendChild(li);
+}
+
+const freundesliste = [
+    
+    {
+        'username': 'fwechsle',
+        'status': 'online'
+    },
+
+    {
+        'username': 'nsassenb',
+        'status': 'online'
+    },
+
+    {
+        'username': 'fheid',
+        'status': 'online'
+    },
+
+    {
+        'username': 'jseidere',
+        'status': 'offline'
+    },
+
+    {
+        'username': 'caigner',
+        'status': 'offline'
+    }
+
+];
+
 export function renderWelcome() {
+
     const app = document.getElementById('app');
 
     app.innerHTML = `
@@ -9,6 +46,20 @@ export function renderWelcome() {
             <div class="menu-friends-container">
                 <div class="menu-friends-header">
                     <p>FRIENDS</p>
+                </div>
+                <hr class="friends-divider">
+                <div id="friendsOnline" class="friends-online">
+                    <div class="friends-online-header">
+                        <p>ONLINE</p>
+                    </div>
+                    <ul id="friendsOnlineList" class="friends-online-list"></ul>
+                </div>
+                <hr class="friends-divider">
+                <div id="friendsOffline" class="friends-offline">
+                    <div class="friends-offline-header">
+                        <p>OFFLINE</p>
+                    </div>
+                    <ul id="friendsOfflineList" class="friends-offline-list"></ul>
                 </div>
             </div>
         </div>
@@ -36,144 +87,16 @@ export function renderWelcome() {
         </div>
     </div>
     `;
-    const friendRequestButton = document.getElementById('sendFriendRequest');
-    const responseDisplay = document.getElementById('responseDisplay');
-    const friendRequests = document.getElementById('friendRequests');
-    const checkRequestsButton = document.getElementById('checkRequests');
-    const acceptedRequests = document.getElementById('acceptedFriend');
-    const acceptedFriendButton = document.getElementById('acceptedFriendButton');
-    const formdata = document.getElementById('matchForm');
-    const messages = document.getElementById('messages');
-    const matchHistoryButton = document.getElementById('matchHistoryButton');
-    const matchHistory = document.getElementById('matchHistory');
 
-    formdata.addEventListener('submit', handleMatchFormSubmit);
-
-    friendRequestButton.addEventListener('click', async () => {
-        const friendId = prompt("Enter the friend's user ID:");
-        if (friendId) {
-            const response = await sendFriendRequest(friendId);
-            displayResponse(response);
-        }
-    });
-
-    checkRequestsButton.addEventListener('click', async () => {
-        const response = await checkRequests();
-        displayFriendResponse(response);
-    })
-
-    acceptedFriendButton.addEventListener('click', async () => {
-        const response = await friendList();
-        displayFriendListResponse(response);
-    })
-
-    matchHistoryButton.addEventListener('click', async () => {
-        const response = await getMatchHistory();
-        displayMatchHistory(response);
-    })
-
-    async function handleMatchFormSubmit(event) {
-        event.preventDefault();
-        const form = event.target;
-        const formData = new FormData(form);
-        const data = {
-            player1_id: formData.get('player1'),
-            player2_id: formData.get('player2'),
-            score_player1: formData.get('score_player1'),
-            score_player2: formData.get('score_player2'),
-        };
+    const friendsOnlineList = document.getElementById('friendsOnlineList');
+    const friendsOfflineList = document.getElementById('friendsOfflineList');
+        
+    for (let i = 0; i < freundesliste.length; i++)
+    {
+        if (freundesliste[i].status === 'online')
+            addListItem(freundesliste[i].username, friendsOnlineList);
+        else
+            addListItem(freundesliste[i].username, friendsOfflineList);
+    }
     
-        const csrftoken = getCookie('csrftoken');
-    
-        const response = await fetch(`/match_result/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrftoken
-            },
-            body: JSON.stringify(data)
-        });
-    
-        const result = await response.json();
-        displayMatchResponse(result);
-    
-    }
-
-    async function sendFriendRequest(friendId) {
-        try {
-            const csrftoken = getCookie('csrftoken');
-            const response = await fetch(`/send_friend_request/${friendId}/`, {
-                method: 'GET',
-                credentials: 'include'
-            });
-            return await response.json();
-        } catch (error) {
-            console.error('Error sending friend request:', error);
-            return { error: 'Failed to send friend request.' };
-        }
-    }
-
-    async function checkRequests() {
-        try {
-            const response = await fetch(`/friend_requests/`, {
-                method: 'GET',
-                credentials: 'include'
-            });
-            return await response.json();
-        } catch (error) {
-            return { error: 'Failed to send friend request.' };
-        }
-    }
-
-    async function friendList() {
-        try {
-            const response = await fetch(`/friend_list/`, {
-                method: 'GET',
-                credentials: 'include'
-            });
-            return await response.json();
-        } catch (error) {
-            return { error: 'Failed to send friend request.' };
-        }
-    }
-
-    async function getMatchHistory() {
-        try {
-            const response = await fetch(`/match_history/`, {
-                method: 'GET',
-                credentials: 'include'
-            });
-            return await response.json();
-        } catch (error) {
-            return { error: 'Failed to get match histroy.' };
-        }
-    }
-
-    function displayResponse(response) {
-        responseDisplay.innerHTML = `
-            <pre>${JSON.stringify(response, null, 2)}</pre>
-        `;
-    }
-
-    function displayFriendResponse(response) {
-        friendRequests.innerHTML = `
-            <pre>${JSON.stringify(response, null, 2)}</pre>
-        `;
-    }
-    function displayFriendListResponse(response) {
-        acceptedRequests.innerHTML = `
-            <pre>${JSON.stringify(response, null, 2)}</pre>
-        `;
-    }
-
-    function displayMatchResponse(response) {
-        messages.innerHTML = `
-            <pre>${JSON.stringify(response, null, 2)}</pre>
-        `;
-    }
-    function displayMatchHistory(response) {
-        matchHistory.innerHTML = `
-            <pre>${JSON.stringify(response, null, 2)}</pre>
-        `;
-    }
 }
