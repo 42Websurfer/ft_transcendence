3eab4e8e3b34722b437c870bf66a345763489e70
windchaser-surf <wechslerf@gmail.com>
2024-09-26 19:38:31 +0000

    FIX: bug in remove friendship

--- app/transendence/user/views.py
+++ app/transendence/user/views.py
@@ -193,6 +193,7 @@ def block_friend_request(request, username):
 			'message': 'Friendship doesn\'t exist.'
 		})
 
+@login_required
 def remove_friendship(request, username):
 	user = request.user
 	friend = get_object_or_404(User, username=username)
