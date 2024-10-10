bb270b8575baa9559f43f894dceeb544548d3fd3
windchaser-surf <wechslerf@gmail.com>
2024-10-10 10:27:26 +0000

    ADD: start_tournament url

--- app/transendence/tournament/urls.py
+++ app/transendence/tournament/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_lobby, name='create'),
-    path('join/<str:lobby_id>/', views.join_lobby, name='join')
+    path('join/<str:lobby_id>/', views.join_lobby, name='join'),
+    path('start_tournament/<str:lobby_id>/', views.start_group_tournament, name='start_tournament'),
 ]
