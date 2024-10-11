e9b4f1214f0589558d9be88933d3736abb15b769
windchaser-surf <wechslerf@gmail.com>
2024-10-11 10:34:53 +0000

    RM: change admin function -> no need

--- app/transendence/tournament/utils.py
+++ app/transendence/tournament/utils.py
@@ -1,10 +1,3 @@
-
-def change_admin(redis, group_name):
-	members = redis.hgetall(group_name)
-	if members:
-		new_admin = next(iter(members))
-		redis.hset(group_name, new_admin, 'admin')
-
 def tournament_string(lobby_id):
 	return (f"tournament_{lobby_id}")
 
