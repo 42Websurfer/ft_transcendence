6e64a97571aaf13d80488493b4e88d48b074e9dd
windchaser-surf <wechslerf@gmail.com>
2024-10-22 12:01:46 +0000

    ADD: path join_online_lobby

--- app/transendence/tournament/urls.py
+++ app/transendence/tournament/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_lobby, name='create'),
-    path('join/<str:lobby_id>/', views.join_lobby, name='join'),
+    path('join_tournament_lobby/<str:lobby_id>/', views.join_lobby, name='join_tournament_lobby'),
     path('start_tournament/<str:lobby_id>/', views.start_group_tournament, name='start_tournament'),
     path('set_match/', views.set_match, name='set_match'),
     path('check_completion/<str:lobby_id>/<int:round>/', views.check_round_completion, name='check_completion'),
     path('bc_update_score/', views.bc_update_score, name="bc_update_score"),
     path('bc_get_score/', views.bc_get_score, name="bc_get_score"),
-    path('bc_delete_score/', views.bc_delete_score, name="bc_delete_score")
+    path('bc_delete_score/', views.bc_delete_score, name="bc_delete_score"),
+    path('join_online_lobby/<str:lobby_id>/', views.join_match_lobby, name='join_online_lobby'),
 ]
