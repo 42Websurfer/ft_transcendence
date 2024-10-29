f587d0231dc6ea8f4fa52b32f5a77f328a121276
windchaser-surf <wechslerf@gmail.com>
2024-10-29 16:51:00 +0100

    FIX: when user change username then username will be also update in gamestatsuser model

--- app/transendence/user/signals.py
+++ app/transendence/user/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import UserProfile
@@ -11,3 +11,17 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
         GameStatsUser.objects.create(user=instance)
+
+@receiver(pre_save, sender=User)
+def update_gamestatsuser(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_user = User.objects.get(pk=instance.pk)
+            if old_user.username != instance.username:
+                gamestatsuser = GameStatsUser.objects.get(user=instance)
+                gamestatsuser.username = instance.username
+                gamestatsuser.save()
+        except User.DoesNotExist:
+            pass
+        except GameStatsUser.DoesNotExist:
+            pass
