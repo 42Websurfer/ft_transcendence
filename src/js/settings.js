9ea17d6bff14316246b5c678c72d4491c2384a52
windchaser-surf <wechslerf@gmail.com>
2024-10-29 16:52:11 +0100

    FIX: implemented the fetch to settings and add the input field for the file upload

--- src/js/settings.js
+++ src/js/settings.js
@@ -7,44 +7,45 @@ export async function renderSettings() {
 	app.innerHTML = `
 	<div class="login">
 		<div class="login-container">
-			<form id="settingsForm">
-				<div id="messages"></div>
+			<form id="settingsForm" enctype="multipart/form-data">
 				<input type="hidden" name="csrfmiddlewaretoken" value="${getCookie('csrftoken')}">
 				<div class="login-instructions">
 					<p>Update your account information!</p>
 				</div>
 				
-
 				<div class="login-form-field">
                     <label for="floatingInput" class="settings-label">Email address</label>
-					<input id="settings-email" type="email" name="settings-email" class="form-control" placeholder="name@example.com">
+					<input id="settings-email" type="email" name="email" class="form-control" placeholder="name@example.com">
 				</div>
 				<div class="login-form-field">
                     <label for="floatingPassword" class="settings-label">New password</label>
-					<input type="password" name="settings-password" class="form-control" placeholder="Password">
+					<input type="password" name="password" class="form-control" placeholder="Password">
 				</div>
 				<div class="login-form-field">
                     <label for="floatingPassword" class="settings-label">Confirm new password</label>
-					<input type="password" name="settings-password" class="form-control" placeholder="Password">
+					<input type="password" name="password" class="form-control" placeholder="Password">
 				</div>
 				<div class="login-form-field">
-                <label for="floatingPassword" class="settings-label">Firstname</label>
-					<input id="settings-firstname" type="text" name="settings-firstname" class="form-control" placeholder="Firstname">
+                    <label for="floatingPassword" class="settings-label">Firstname</label>
+					<input id="settings-firstname" type="text" name="firstname" class="form-control" placeholder="Firstname">
 				</div>
 				<div class="login-form-field">
-                <label for="floatingPassword" class="settings-label">Lastname</label>
-					<input id="settings-lastname" type="text" name="settings-lastname" class="form-control" placeholder="Lastname">
+                	<label for="floatingPassword" class="settings-label">Lastname</label>
+					<input id="settings-lastname" type="text" name="lastname" class="form-control" placeholder="Lastname">
 				</div>
 				<div class="login-form-field">
-                <label for="floatingPassword" class="settings-label">Username</label>
-					<input id="settings-username" type="text" name="settings-username" class="form-control" placeholder="Username">
+                    <label for="floatingPassword" class="settings-label">Username</label>
+					<input id="settings-username" type="text" name="username" class="form-control" placeholder="Username">
 				</div>
-
-
-				<button class="signin-button btn btn-primary w-100 py-2" type="submit">Update</button>
-				
+				<div class="login-form-field form-floating">
+					<input type="file" name="avatar" class="form-control" placeholder="Upload avatar">
+					<label for="floatingInput">Upload avatar</label>
+				</div>	
+				<div id="settingsMessage" class="register-message"></div>
+				<button class="signin-button btn btn-primary w-100 py-2" type="submit">Update</button>	
 			</form>
 		</div>
+		<div id="settingsLoader" class="loader"></div> 	
 	</div>
 	`;
     const inputEmail = document.getElementById('settings-email');
@@ -62,8 +63,8 @@ export async function renderSettings() {
         inputLastname.value = response.lastname;
         inputUsername.value = response.username;
     }    
-	const form = document.getElementById('registerForm');
-    form.addEventListener('submit', handleFormSubmit);
+	const form = document.getElementById('settingsForm');
+    form.addEventListener('submit', handleSettingsFormSubmit);
 
 }
 
@@ -81,31 +82,44 @@ async function getUserInformation() {
     }
 }
 
-async function handleFormSubmit(event) {
+async function handleSettingsFormSubmit(event) {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
-    const data = {
-        email: formData.get('register-email'),
-        password: formData.get('register-password'),
-        firstname: formData.get('register-firstname'),
-        lastname: formData.get('register-lastname'),
-				username: formData.get('register-username')
-    };
-
+	
     const csrftoken = getCookie('csrftoken');
-
-    const response = await fetch('/register/', {
-        method: 'POST',
+	
+    const response = await fetch('/settings/', {
+		method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
             'X-CSRFToken': csrftoken
         },
-        body: JSON.stringify(data)
+        body: formData//JSON.stringify(data)
     });
-
+	
     const result = await response.json();
-    displayMessages(result);
-	showSection('auth_login');
+	console.log("result: ", result);
+
+	const settingsMessage = document.getElementById('settingsMessage');
+	
+	if (result.type === 'success')
+	{
+		settingsMessage.textContent = '';
+		const buttons = document.querySelectorAll('button');
+		buttons.forEach(button => button.disabled = true);
+
+		const settingsLoader = document.getElementById('settingsLoader');
+		settingsLoader.style.display = 'block';
+
+		setTimeout(() => showSection('menu'), 2000);
+	}
+	else
+	{
+		settingsMessage.textContent = result.message;
+		settingsMessage.style.color = 'red';
+		settingsMessage.style.animation = 'none';
+		settingsMessage.offsetHeight;
+		settingsMessage.style.animation = 'wiggle 0.5s ease-in-out';
+	}
 
 }
