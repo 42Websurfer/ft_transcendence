5c040ed2cc6259a1cb63eae3902075b0e85886e8
windchaser-surf <wechslerf@gmail.com>
2024-11-10 16:42:10 +0000

    FIX: add user object in response

--- app/transendence/user/views.py
+++ app/transendence/user/views.py
@@ -436,6 +436,13 @@ def api_callback(request):
                 {
                     'type': 'success',
                     'message': 'User registered successfully.',
+                    'user': {
+                        'id': user.id,
+                        'username': user.username,
+                        'email': user.email,
+                        'first_name': user.first_name,
+                        'last_name': user.last_name
+                    },
                     'session_data': session_data,
 
                 }, status=200)
