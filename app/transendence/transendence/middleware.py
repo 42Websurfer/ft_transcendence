56e78593ed237fb6c48378a82e7c0f3cb2419aaa
windchaser-surf <wechslerf@gmail.com>
2024-11-13 10:30:08 +0100

    RM: debugging prints

--- app/transendence/transendence/middleware.py
+++ app/transendence/transendence/middleware.py
@@ -12,7 +12,6 @@ User = get_user_model()
 @database_sync_to_async
 def get_user(user_id):
     try:
-        print("VIELLEICHT BEKKOMEN WIR EINE USER_ID = ", user_id)
         return User.objects.get(id=user_id)
     except User.DoesNotExist:
         return AnonymousUser()
@@ -21,17 +20,12 @@ class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_string = parse_qs(scope["query_string"].decode())
         token = query_string.get("token", [None])[0]
-        print("TOKEN = ", token)
         if token:
             try:
-                print("\nFAIL?\n")
-                print("SECRET KEY = ", settings.SECRET_KEY)
                 decoded_data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-                print("DECODED_DATA = ", decoded_data)
                 user_id = decoded_data.get("user_id")
                 scope["user"] = await get_user(user_id)
             except (InvalidToken, TokenError) as e:
-                print("Invalid token or token error:", e)
                 scope["user"] = AnonymousUser()
         else:
             scope["user"] = AnonymousUser()
