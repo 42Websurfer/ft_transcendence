b6fd2ecb5e5a1bea44888f113591edda7ac474b8
windchaser-surf <wechslerf@gmail.com>
2024-11-14 14:55:21 +0000

    FIX: some problems with starting the container, set some depends on that not everything start at the same time.

--- .devcontainer/tools/init_django.sh
+++ .devcontainer/tools/init_django.sh
@@ -6,19 +6,16 @@ source /workspaces/app/webappenv/bin/activate
 
 pip install --upgrade setuptools
 pip install django psycopg2-binary
-/workspaces/app/transendence/manage.py makemigrations
-/workspaces/app/transendence/manage.py migrate
 pip install whitenoise
 pip install channels
 pip install daphne
 pip install channels_redis
 pip install requests
 pip install Pillow
-pip install django-otp
-pip install pyotp
-pip install qrcode
 pip install djangorestframework 
 pip install djangorestframework-simplejwt
+/workspaces/app/transendence/manage.py makemigrations-
+/workspaces/app/transendence/manage.py migrate
 export PYTHONPATH=/workspaces/app/transendence
 daphne -p 8001 -b 0.0.0.0 transendence.asgi:application &
 python3 /workspaces/app/transendence/manage.py runserver 0.0.0.0:8000
--- app/transendence/transendence/urls.py
+++ app/transendence/transendence/urls.py
@@ -25,7 +25,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     #path('', include('user.urls')),
     path('admin/', admin.site.urls),
-    path('tm/', include('tournament.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
