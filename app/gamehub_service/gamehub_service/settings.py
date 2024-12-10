bef1ecd63a2a05f5fd8d925cfc90af62ed723c58
windchaser-surf <wechslerf@gmail.com>
2024-12-10 18:33:59 +0100

    FIX: added new host to allowed hosts

--- app/gamehub_service/gamehub_service/settings.py
+++ app/gamehub_service/gamehub_service/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'development', 'user-service', 'gamehub-service', '10.13.9.6', '127.0.0.1', '10.13.9.3', '10.13.9.2', '10.14.7.4', '10.14.7.3', '10.14.1.9', '10.14.3.9', 'websurfers.com']
+ALLOWED_HOSTS = ['localhost', 'development', '10.13.9.1', 'user-service', 'gamehub-service', '10.13.9.6', '127.0.0.1', '10.13.9.3', '10.13.9.2', '10.14.7.4', '10.14.7.3', '10.14.1.9', '10.14.3.9', 'websurfers.com']
 
 
 # Application definition
