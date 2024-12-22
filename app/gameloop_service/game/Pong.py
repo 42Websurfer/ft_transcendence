843eb195b0700e122fc6651fbe6a81a9c4deef31
Niko <144370425+Nikster3k@users.noreply.github.com>
2024-12-22 17:10:28 +0100

    This gives all the movement updates to the asyncio thread each world loop iteration, and then it will handle sending to the consumers

--- app/gameloop_service/game/Pong.py
+++ app/gameloop_service/game/Pong.py
@@ -304,6 +304,8 @@ class PongGame:
 		self.asyncio_thread = None
 		self.game_thread = None
 
+		self.move_tasks = []
+
 	def set_players(self, group_name):
 		self.players = GamesHandler.game_players(group_name)
 
@@ -379,20 +381,28 @@ class PongGame:
 		thread_local.event_loop = self.event_loop
 		thread_local.host = self.players[0]
 		thread_local.world = self.world
-		iter = 0
+
 		while not self.stop_thread:
 			self.world.update()
+			asyncio.run_coroutine_threadsafe(self.send_move_tasks(), self.event_loop)
 			time.sleep(0.016)
-			if iter == 1000:
-				print('game running on', self.players[0].group_name)
-				iter = 0
-			iter += 1
+			
 		print('game loop stopped of group', self.players[0].group_name if len(self.players) != 0 else '[Removed]')
 		self.event_loop.call_soon_threadsafe(self.event_loop.stop)
 		print('asyncio event_loop ordered to stop')
 		for player in self.players:
 			(async_to_sync)(player.close)()
-
+	
+	async def send_move_tasks(self):
+		"""
+		Instead of trying to send moves instantly we save the moves we want to send and then send them in a batch
+		"""
+		gather = [consumer.send(move) for consumer in self.players for move in self.move_tasks]
+		self.move_tasks.clear()
+		if len(gather) == 0:
+			return
+		await asyncio.gather(*gather)
+	
 	def asyncio_tasks_thread(self):
 		asyncio.set_event_loop(self.event_loop)
 		self.event_loop.run_forever()
@@ -401,7 +411,9 @@ class PongGame:
 	"""
 	Some big and commonly used sends defined here to make code more readable
 	"""
-	def send_entity_move(self, entity):
+	def send_entity_move(self, entity: Entity):
+		self.move_tasks.append(f"up;{entity.id};{entity.position.x};{entity.position.y};{entity.rotation}")
+		return
 		asyncio.run_coroutine_threadsafe(self.players[0].channel_layer.group_send(
 			self.players[0].group_name,
 			{
@@ -413,6 +425,8 @@ class PongGame:
 			self.event_loop)
 		
 	def send_entity_set_pos(self, entity):
+		self.move_tasks.append(f"sp;{entity.id};{entity.position.x};{entity.position.y};{entity.rotation}")
+		return
 		asyncio.run_coroutine_threadsafe(self.players[0].channel_layer.group_send(
 			self.players[0].group_name,
 			{
